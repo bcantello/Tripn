3296c393b69398c19064691f99411a57c8d0df35
Brandon Cantello <bcantello@gmail.com>
2020-04-02 16:56:35 -0700

    Adds media queries to ensure Tablet and Desktop maintain mobile sizing

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function About() {
     return(
-    <div>
+    <div id={'about-container'}>
         <p>This page will have a description of what the app does, as well as images and a walkthrough of how to use it.</p>
     </div>
     )
