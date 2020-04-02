b9ab02287fc77f0412d11f0b78ef4285fcc202ff
Brandon Cantello <bcantello@gmail.com>
2020-04-02 16:02:16 -0700

    updates styling for directions component

--- src/Components/Directions/Directions.js
+++ src/Components/Directions/Directions.js
@@ -1,10 +1,14 @@
 import React, {useContext} from "react";
 import DirectionsList from "./DirectionsList";
 import {UniversalProps} from "../../App";
+import './directionsStyles.css'
 
 export default function Directions() {
     const universalProps = useContext(UniversalProps);
     return(
-        <DirectionsList directions={universalProps.directions}/>
+        <div>
+            <div id={'end-directions-placeholder'}></div>
+            <DirectionsList directions={universalProps.directions}/>
+        </div>
     )
 }
