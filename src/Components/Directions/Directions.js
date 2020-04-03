42d11bdbadd388adbb995af94e071bf7bc88b99c
Brandon Cantello <bcantello@gmail.com>
2020-04-03 00:25:12 -0700

    adds previous directions button

--- src/Components/Directions/Directions.js
+++ src/Components/Directions/Directions.js
@@ -7,7 +7,9 @@ export default function Directions() {
     const universalProps = useContext(UniversalProps);
     return(
         <div>
-            <div id={'end-directions-placeholder'}></div>
+            <div id={'end-directions-placeholder'} style={{display: 'none'}}>
+                Looks like you're done Tripn. Time to start planning your next adventure!
+            </div>
             <DirectionsList directions={universalProps.directions}/>
         </div>
     )
--- src/Components/Directions/DirectionsList.js
+++ src/Components/Directions/DirectionsList.js
@@ -6,9 +6,27 @@ export default function DirectionsList({directions}) {
     let directionsListArr = this;
     const [count, setCount] = useState(0);
     let destination = this;
+    const container = document.getElementById('end-directions-placeholder');
 
-    const handleClick = () => {
-        setCount(count + 1);
+    const handleCountIncrement = () => {
+        if (count < directions['routes'][0]['legs'].length) {
+            setCount(count + 1);
+            let tempCount = count + 1;
+            if (tempCount === directions['routes'][0]['legs'].length) {
+                container.style.display = "block";
+            }
+        }
+    };
+
+    const handleCountDecrement = () => {
+
+        if (count > 0) {
+            setCount(count - 1);
+            let tempCount = count - 1;
+            if (tempCount < directions['routes'][0]['legs'].length) {
+                container.style.display = "none";
+            }
+        }
     };
 
     if (directions['routes'] !== undefined) {
@@ -20,21 +38,14 @@ export default function DirectionsList({directions}) {
                     <DirectionsListItem element={e} key={index}/>
                 );
             });
-        } else {
-           const container = document.getElementById('end-directions-placeholder');
-            let message = document.createElement('DIV');
-            let content = document.createTextNode('Looks like you\'re done Tripn. Time to start planning your next adventure!');
-            message.appendChild(content);
-            container.appendChild(message);
-            let nextButton = document.getElementById('directions-button');
-            nextButton.remove();
         }
     }
     return(
         <div id={'directions-list-container'}>
             <div className={'destination-element'}>{destination}</div>
             <div className="directions-list">{directionsListArr}</div>
-            <button id={'directions-button'} onClick={handleClick}>Next Destination</button>
+            <button id={'directions-button'} onClick={handleCountDecrement}>Previous Destination</button>
+            <button id={'directions-button'} onClick={handleCountIncrement}>Next Destination</button>
         </div>
     )
 }
