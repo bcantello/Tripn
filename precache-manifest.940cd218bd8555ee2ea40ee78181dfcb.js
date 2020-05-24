476c0132cd8438cdf18fc8406e990add513803a0
Brandon Cantello <bcantello@gmail.com>
2020-05-23 21:18:20 -0700

    Updates

---
+++ precache-manifest.940cd218bd8555ee2ea40ee78181dfcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c8aa542eff7a3cc377d2ff181a9047",
+    "url": "/Tripn/index.html"
+  },
+  {
+    "revision": "007e5e019659575d1ec1",
+    "url": "/Tripn/static/css/main.ffa76482.chunk.css"
+  },
+  {
+    "revision": "3f9f76d4736fc54e0e4b",
+    "url": "/Tripn/static/js/2.8c5e684d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Tripn/static/js/2.8c5e684d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "007e5e019659575d1ec1",
+    "url": "/Tripn/static/js/main.bf23bfc6.chunk.js"
+  },
+  {
+    "revision": "6b01079cd46ac0c03906",
+    "url": "/Tripn/static/js/runtime-main.bda27251.js"
+  }
+]);
